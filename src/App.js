70cd7925d2c40c91a7f65591bdad9125999f6371
Syed Muhammed Bilal <intergo878@gmail.com>
2020-04-23 00:46:40 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import Greet from './components/greet.component'
 import Welcome from './components/welcome.component'
 import Message from './components/message.component'
+import Counter from './components/counter.component'
 
 function App() {
   return (
     <div className="App">
+    <Counter />
     {/* props practice */}
       {/*<Greet name='Bilal' skills='programmer' >
         <p>this is childern props</p>
       </Greet>
       <Greet name='Mani' skills='programmer' />
       <Greet name='Wasiq' skills='buissness' />
-      <Welcome name='Bilal' skills='programmer' />*/}
-      <Message />
+      <Welcome name='Bilal' skills='programmer' />
+      <Message />*/}
     </div>
   );
 }
