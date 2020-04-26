340cb1525b9bb3cec1b084372eb0d92e3968c72b
Syed Muhammed Bilal <intergo878@gmail.com>
2020-04-26 14:17:07 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router,
+Switch, 
+Route, } from 'react-router-dom'
+
 import './App.css';
 import Greet from './components/greet.component'
 import Welcome from './components/welcome.component'
@@ -6,12 +10,35 @@ import Message from './components/message.component'
 import Counter from './components/counter.component'
 import FuncClick from './components/funcClick.component'
 import ClassClick from './components/ClassClick.component'
+import EventBind from './components/EventBind.component'
+import Parent from './components/parent.component'
+import UserGreeting from './components/usergreeting.component'
+import NameList from './components/NameList.component'
+import StyleSheet from './components/StyleSheet'
+import Inline from './components/Inline'
+import Form from './components/form'
+import user from './user'
 
 function App() {
   return (
-    <div className="App">
-    <FuncClick />
-    <ClassClick />
+    
+      <div className="App">
+      <Form />
+      {/*<StyleSheet primary={true}/>
+      <Inline />*/}
+        {/*<NameList />*/}
+        {/*<Router>
+          <Switch>
+            <Route path='/user' exact component={user} />
+          </Switch>
+        </Router>*/}
+        
+    {/*<Link to={'/user'}>user</Link>*/}
+    {/*<UserGreeting />*/}
+    {/*<Parent />*/}
+    {/*<EventBind />*/}
+    {/*<FuncClick />
+    <ClassClick />*/}
     {/*<Counter />*/}
     {/* props practice */}
       {/*<Greet name='Bilal' skills='programmer' >
@@ -21,8 +48,10 @@ function App() {
       <Greet name='Wasiq' skills='buissness' />
       <Welcome name='Bilal' skills='programmer' />
       <Message />*/}
-    </div>
+      </div>
+   
   );
 }
+
 
 export default App;
